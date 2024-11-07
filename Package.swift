f039db928c3f88f47c785566f7f190376e5c5088
Paul Brenner <paul-brenner@users.noreply.github.com>
2024-11-07 19:47:05 +0900

    Update Package.swift
    update url for swift-syntax

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         ),
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-syntax.git", from: "600.0.0"),
+        .package(url: "https://github.com/swiftlang/swift-syntax", "509.0.0"..<"601.0.0-prerelease"),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
